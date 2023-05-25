cfa9b030db7ffbe127e234b4f00a98202d00c9a3
zhuzedan <1031155817@qq.com>
2023-05-25 09:58:27 +0800

    fix:error

--- server/src/main/java/org/zzd/mapper/SystemMenuMapper.java
+++ server/src/main/java/org/zzd/mapper/SystemMenuMapper.java
@@ -1,6 +1,7 @@
 package org.zzd.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 import org.zzd.entity.SystemMenu;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
  * @author zzd
  * @since 2023-03-13 14:26:10
  */
+@Repository
 public interface SystemMenuMapper extends BaseMapper<SystemMenu> {
     List<SystemMenu> getSystemUserMenuList(Long userId);
     List<SystemMenu> getAsideMenuList(Long userId);
