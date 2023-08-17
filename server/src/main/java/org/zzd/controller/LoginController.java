0f744ea93e13a029b4bbdd603b668f911d481e5e
zhuzedan <1031155817@qq.com>
2023-08-17 09:47:02 +0800

    feat:自定义配置文件

--- server/src/main/java/org/zzd/controller/LoginController.java
+++ server/src/main/java/org/zzd/controller/LoginController.java
@@ -1,24 +1,18 @@
 package org.zzd.controller;
 
-import com.wf.captcha.base.Captcha;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.zzd.annotation.LoginLog;
-import org.zzd.bean.LoginCodeEnum;
-import org.zzd.bean.LoginProperties;
 import org.zzd.dto.user.LoginCaptchaDto;
 import org.zzd.dto.user.LoginDto;
 import org.zzd.result.ResponseResult;
+import org.zzd.service.EasyCaptchaService;
 import org.zzd.service.SystemUserService;
 import org.zzd.service.WeChatService;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @apiNote 登录相关接口
@@ -33,43 +27,31 @@ public class LoginController {
     SystemUserService systemUserService;
     @Autowired
     private WeChatService weChatService;
-    @Resource
-    private LoginProperties loginProperties;
-
-    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    private EasyCaptchaService easyCaptchaService;
 
     @LoginLog
     @ApiOperation("用户登录")
     @PostMapping("/login")
-    public ResponseResult login(@RequestBody LoginDto loginDto) {
+    public ResponseResult<?> login(@RequestBody LoginDto loginDto) {
         return systemUserService.login(loginDto);
     }
 
     @LoginLog
     @ApiOperation("用户登录（captcha）")
     @PostMapping("/loginCaptcha")
-    public ResponseResult loginAndCaptcha(@RequestBody LoginCaptchaDto loginCaptchaDto, HttpServletRequest request) {
+    public ResponseResult<?> loginAndCaptcha(@RequestBody LoginCaptchaDto loginCaptchaDto, HttpServletRequest request) {
         return systemUserService.loginCaptcha(loginCaptchaDto, request);
     }
 
     @ApiOperation(value = "获取验证码")
     @GetMapping(value = "/captcha")
-    public Object getCode(HttpServletRequest request, HttpServletResponse response) {
-        Captcha captcha = loginProperties.getCaptcha();
-        //当验证码类型为 arithmetic时且长度 >= 2 时，captcha.text()的结果有几率为浮点型
-        String captchaValue = captcha.text();
-        if (captcha.getCharType() - 1 == LoginCodeEnum.ARITHMETIC.ordinal() && captchaValue.contains(".")) {
-            captchaValue = captchaValue.split("\\.")[0];
-        }
-        logger.info("图片验证码结果是" + captchaValue);
-        // 保存到session
-        request.getSession().setAttribute("captcha", captchaValue);
-        // 验证码信息
-        return captcha.toBase64();
+    public ResponseResult<?> getCode() {
+        return ResponseResult.success(easyCaptchaService.getCaptchaValueAndBase64(null));
     }
 
     @GetMapping("/wxLogin")
-    public ResponseResult wxLogin(String code) {
+    public ResponseResult<?> wxLogin(String code) {
         return weChatService.wxLogin(code);
     }
 
